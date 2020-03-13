76acf9ae63bdbfca035bd102edb270654aeb321e
ondrejch <ondrejch@kentico.com>
2020-03-13 17:40:55 +0100

    format

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
-    "ts-jest": {
-      compiler: "ttypescript"
-    }
+    'ts-jest': {
+      compiler: 'ttypescript',
+    },
   },
-  setupFiles: [
-    "<rootDir>jest-ts-auto-mock.config.ts"
-  ]
+  setupFiles: ['<rootDir>jest-ts-auto-mock.config.ts'],
 };
